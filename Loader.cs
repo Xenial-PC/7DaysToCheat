3c52f9430ac56318b87d0668dfd269930f41952a
Xenial-PC <58645774+Xenial-PC@users.noreply.github.com>
2020-12-25 21:35:17 -0500

    init commit

---
+++ Loader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace _7DaysToCheat
+{
+    public class Loader : MonoBehaviour
+    {
+        private static GameObject _injectGameObject;
+
+        public static void Load()
+        {
+            _injectGameObject = new GameObject();
+            _injectGameObject.AddComponent<Main>();
+
+            DontDestroyOnLoad(_injectGameObject);
+        }
+
+        public static void Destroy()
+        {
+            Destroy(_injectGameObject);
+        }
+    }
+}
